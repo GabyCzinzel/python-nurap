e896739cd5fb2726d09bddec3814d4daa092adb3
GabyCzinzel <gabrielaczinzel@gmail.com>
2024-12-03 09:50:47 -0300

    Add files via upload
    aula 2

---
+++ Aula 2/aula02.py
@@ -0,0 +1,8 @@
+print("gosto de comer")
+
+nome_usuario = 'Gabriella Czinzel' # tipo -> str = string 
+idade_usuario = 15 # tipo -> int = integer 
+altura_usuario = 1,62 # tipo -> float = ponto flutuante 
+maior_idade = False # tipo -> bool = boolean
+
+print (("seja bem vindo"), nome_usuario, ('sua idade é?'), idade_usuario, ("anos, voce tem"), altura_usuario, ('metros. você é maior de idade?'), maior_idade)
